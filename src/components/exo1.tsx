c04fe3e653d4cb2a4696fc8b53c5b0bc2a00eec6
Younes <younes.moumtaz@gmail.com>
2023-04-04 14:32:25 +0200

    exo1 correction

--- src/components/exo1.tsx
+++ src/components/exo1.tsx
@@ -1,3 +1,5 @@
+import PropTypes from "prop-types"
+import style from "./exo1.module.css"
 
 interface Props {
     name: string;
@@ -8,10 +10,20 @@ const Exo1= ({name, age} : Props) => {
 
     return (
         <>
-            <p>Bienvenue {name} sur l'application React!</p>    
-            <p>Vous avez {age} ans !</p>        
+            <p className={style.name}>Bienvenue {name} sur l'application React!</p>    
+            <p className={style.age}>Vous avez {age} ans !</p>        
         </>
     )   
+}
+
+Exo1.defaultProps= {
+    name: "Younes",
+    age: 26
+}
+
+Exo1.propTypes= {
+    name: PropTypes.string.isRequired,
+    age: PropTypes.number.isRequired
 }
 
 export default Exo1;
